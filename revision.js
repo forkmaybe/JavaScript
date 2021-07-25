ee43b36c91927245da10685732f3033ff4e47b71
forkmaybe <gossluke@hotmail.com>
2021-07-25 15:36:21 +0200

    Create revision.js

---
+++ revision.js
@@ -0,0 +1,117 @@
+//Pluralsight
+//Strings
+'This is a string.';
+"This is the 2nd string.";
+
+"It's six o'clock.";
+'Remember to say "please" and "thank you."';
+
+'It\'s six o\'clock.';
+"Remember to say \"please\" and \"thank you.\"";
+
+"caterpillar".length;
+
+"THE KIDS".toLowerCase();
+
+"I wish I were big.".toUpperCase();
+
+"   but keep the middle spaces   ".trim();
+
+//numbers
+12345;
+10 + 3.14159;
+1 / 3;
+11 / 10;
+1 + (4 / 3);
+-3;
+5 - 7;
+
+//booleans
+var kitchenLights = false;
+kitchenLights = true;
+kitchenLights;
+
+//operators
+1 + 2;
+50 - 15;
+3 * 12;
+12 / 4;
+1 + 100 * 5;
+(1 + 100) * 5;
+"news" + "paper";
+var dinner = "sushi";
+
+//variables
+var x = 100;
+x + 102;
+var x = 100;
+var y = x + 102;
+y;
+//Outputs 202
+var weather = "rainy";
+weather = "sunny";
+weather;
+var camelCase = "lowercase word, then uppercase";
+var dinner2Go = "pizza";
+var I_AM_HUNGRY = true;
+var _Hello_ = "what a nice greeting"
+var $_$ = "money eyes";
+
+//functions
+function addTwoNumbers(x, y) {
+       return x + y;
+}
+
+function greetThePlanet() {
+       return "Hello world!";
+}
+greetThePlanet();
+
+function square(number) {
+       return number * number;
+}
+square(16);
+
+//conditionals
+if (10 > 5) {
+      var outcome = "if block";
+}
+outcome;
+
+if ("cat" === "dog") {
+      var outcome = "if block";
+} else {
+      var outcome = "else block";
+}
+outcome;
+
+if (false) {
+      var outcome = "if block";
+} else if (true) {
+      var outcome = "else if block";
+} else {
+      var outcome = "else block";
+}
+outcome;
+
+if (false) {
+      var outcome = "if block";
+} else if (true) {
+      var outcome = "first else if block";
+} else if (true) {
+      var outcome = "second else if block";
+} else {
+      var outcome = "else block";
+}
+outcome;
+
+if (false) {
+      var outcome = "if block";
+} else if (false) {
+      var outcome = "else if block";
+}
+outcome;
+
+//Arrays
+var breakfast = ["coffee", "croissant"];
+breakfast;
